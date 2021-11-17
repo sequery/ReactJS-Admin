c0a6641cb5d480f3af0a3101b2161579fd8374b2
sequery <foreducation9800@gmail.com>
2021-11-17 13:02:49 +0500

    finished navbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,22 +1,32 @@
 import './topbar.scss'
-import {NotificationsNone} from '@material-ui/icons'
+import { NotificationsNone, Language, Settings } from '@material-ui/icons'
+import profile_pic from '../../assets/profile.jpeg'
 
 export const Topbar = () => {
     return (
         <div className="topbar">
-        <div className="topbarWrapper">
-            <div className="topLeft">
-                <span className="logo">
-                    SequeryAdmin
-                </span>
-            </div>
-            <div className="topRight">
-                <div className="topbarIcons">
-                    <NotificationsNone />
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">
+                        SequeryAdmin
+                    </span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIcons">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIcons">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIcons">
+                        <Settings />
+                    </div>
+                    <img src={profile_pic} alt="" className="topAvatar" />
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
