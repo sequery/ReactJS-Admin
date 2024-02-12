d74d4e86511fe1bf38869b35d665488256d62443
sequery <agageldi.artykov@gmail.com>
2024-02-12 06:59:14 -0800

    Add User page route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Topbar from './components/topbar/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './pages/home/Home';
 import UserList from './pages/userList/UserList';
+import User from "./pages/user/User";
+
 
 import './app.scss';
 
@@ -16,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="/users" element={<UserList />}></Route>
+          <Route path="/user/:userId" element={<User />}></Route>
         </Routes>
       </div>
     </Router>);
